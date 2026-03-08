727d148385287866910637ea5ea2e6bbfdac2a0f
Jaweriagoraya <jaweria.nasir@icloud.com>
2026-03-08 17:09:15 +0500

    assignment2

---
+++ assignment2/script.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("menu-toggle");
+const menu = document.querySelector(".nav-left");
+
+toggle.addEventListener("click", function() {
+
+menu.classList.toggle("active");
+
+});
