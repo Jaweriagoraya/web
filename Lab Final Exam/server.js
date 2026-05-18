2d7bc164080ab6db1dfcd1cfa50e44e3b4bfde90
Jaweriagoraya <jaweria.nasir@icloud.com>
2026-05-18 23:49:57 +0500

    Updated Lab Final Exam

--- Lab Final Exam/server.js
+++ Lab Final Exam/server.js
@@ -1,10 +1,10 @@
 // This file is just a helper to start the actual server.
 // It changes the directory to the correct folder and starts the app.
 
-console.log("Starting server from the 'Assignment 04 LAB web' folder...");
+console.log("Starting server from the 'final lab' folder...");
 
 // Change the current working directory to where the actual code is
-process.chdir(__dirname + '/Assignment 04 LAB web');
+process.chdir(__dirname + '/final lab');
 
 // Require the actual server script located in the inner folder
-require('./Assignment 04 LAB web/server.js');
+require('./final lab/server.js');
